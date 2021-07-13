58d99a66847141fc55836e57214ac39df32d031b
vibhanshsingh <vibhansh.singh568@gmail.com>
2021-07-13 21:22:07 +0530

    restoring project

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,116 +1,56 @@
-import React, {useState} from 'react';
-import AddSubscribers from './AddSubscribers';
-import ShowSubscribers from "./ShowSubscribers";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-// import {useHistory} from 'react-router-dom';
+import React, {Component, useState} from 'react';
+import AddSubscriber from './AddSubscriber';
+import ShowSubscribers from './ShowSubscribers';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-export default function PhoneDirectory() {
+export default function PhoneDirectory(){
 
-        const[subscribersList, setSubscribersList] = useState([
-            {
-            id: 1,
-            name: 'Shilpa Bhat',
-            phone: '99999999'
-            },
+            const [subscribersList,setSubscribersList] = useState([
+                {
+                    id: 1,
+                    name: "Shilpa Bhat",
+                    phone: "9999999999"
+                },
+                {
+                    id: 2,
+                    name: "Srishti Gupta",
+                    phone: "8888888888"
+                }
+            ]);
 
-            {
-                id: 2,
-                name: 'Srishti Gupta',
-                phone: '888888888'
-            }]);
 
-    function deleteSubscriberHandler (subscriberID) {
 
-        const newSubscribers = subscribersList.filter((subscriber)=>subscriber.id !== subscriberID) ;
-        setSubscribersList(newSubscribers);
+   function deleteSubscriberHandler (subscriberId)  {
+
+        const newSubscribers = subscribersList.filter((subscriber)=>subscriber.id !== subscriberId);
+
+       setSubscribersList(newSubscribers)
     }
 
-    function addSubscriberHandler (newSubscriber) {
-        if(subscribersList.length > 0) {
-            newSubscriber.id = subscribersList[subscribersList.length-1].id + 1;
-        }
-        else {
+    function addSubscriberHandler  (newSubscriber) {
+
+        if (subscribersList.length > 0) {
+            newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+        } else {
             newSubscriber.id = 1;
-            }
+        }
         subscribersList.push(newSubscriber);
-        setSubscribersList(subscribersList);
-        console.log("Phone directory");
-        console.log(subscribersList);
+        setSubscribersList(subscribersList)
+        //this.setState({ subscribersList: subscribersList });
     }
 
 
     return (
         <Router>
-            <div className={"main-container"}>
-                <Route exact path='/' render = {(props) => <ShowSubscribers {...props} subscribersList={subscribersList} deleteSubscriberHandler={(subscriberId) => deleteSubscriberHandler(subscriberId)}/>}/>
-                <Route exact path='/add' render = {({history}, props) => <AddSubscribers {...props} addSubscriberHandler={(newSubscriber)=>addSubscriberHandler(newSubscriber)}/>}/>
+            <div>
+                <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={subscribersList} deleteSubscriberHandler={(subscriberId)=>deleteSubscriberHandler(subscriberId)} />} />
+                <Route exact path="/add" render={({history}, props) => <AddSubscriber {...props} addSubscriberHandler={(newSubscriber)=>addSubscriberHandler(newSubscriber)} />} />
             </div>
         </Router>
     )
 
 }
-
-
 //
-// class PhoneDirectory extends Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             subscribersList: [{
-//                 id: 1,
-//                 name: 'Shilpa Bhat',
-//                 phone: '99999999'
-//             },
-//                 {
-//                  id: 2,
-//                  name: 'Srishti Gupta',
-//                  phone: '888888888'
-//                 }]
-//         }
-//     }
-//
-//     addSubscriberHandler = (newSubscriber) => {
-//         let subscribersList = this.state.subscribersList;
-//         if(subscribersList.length > 0) {
-//             newSubscriber.id = subscribersList[subscribersList.length-1].id + 1;
-//         }
-//         else {
-//             newSubscriber.id = 1;
-//             }
-//         subscribersList.push(newSubscriber);
-//         this.setState({subscribersList: subscribersList});
-//         console.log("Phone directory");
-//         console.log(this.state.subscribersList);
-//     }
-//
-//     deleteSubscriberHandler = (subscriberID) => {
-//         let subscribersList = this.state.subscribersList;
-//         let subscriberIndex = 0;
-//         subscribersList.forEach(function (subscriber,index){
-//             if (subscriber.id === subscriberID) {
-//                 subscriberIndex = index;
-//             }
-//         },this);
-//         let newSubscribers = subscribersList;
-//         newSubscribers.splice(subscriberIndex, 1);
-//         this.setState({subscribers: newSubscribers});
-//     }
-//
-//     render() {
-//         return (
-//             <Router>
-//                 <div className={"main-container"}>
-//                 <Route exact path='/' render = {(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler}/>}/>
-//                 <Route exact path='/add' render = {({history}, props) => <AddSubscribers history={history} {...props} addSubscriberHandler={this.addSubscriberHandler}/>}/>
-//                 </div>
-//             </Router>
-//         )
-//     }
-// }
-//
-// export default PhoneDirectory;
-//
-
 // class PhoneDirectory extends Component {
 //
 //     constructor() {
@@ -166,3 +106,5 @@ export default function PhoneDirectory() {
 //         )
 //     }
 // }
+//
+// export default PhoneDirectory;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import './common/common.css';
-import PhoneDirectory from "./PhoneDirectory";
-
-// import AddSubscriber from "./AddSubscribers";
-// import AddSubscribers from "./AddSubscribers";
+import PhoneDirectory from './PhoneDirectory';
 
 ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
-
 registerServiceWorker();
